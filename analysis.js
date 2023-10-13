e27c0a0be23a97d0d8a647dfed9907ce59ec7580
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2023-10-13 11:43:28 +0200

    fix(data) - resolve some issue allocate data

--- analysis.js
+++ analysis.js
@@ -24,24 +24,25 @@ function updateTabUrlBar(tabId, grade, score, requests) {
     browser.pageAction.show(tabId);
 }
 
-browser.pageAction.onClicked.addListener((tab) => {
-    // Hide the page action icon for the current tab
-    browser.pageAction.hide(tab.id);
+if(browser.pageAction){
+    browser.pageAction.onClicked.addListener((tab) => {
+        // Hide the page action icon for the current tab
+        browser.pageAction.hide(tab.id);
 
-    // Define the URL you want to open in the new tab
-    const ECO_URL = "https://bff.ecoindex.fr/redirect/?url=";
+        // Define the URL you want to open in the new tab
+        const ECO_URL = "https://bff.ecoindex.fr/redirect/?url=";
 
-    // Get the current tab's URL and pass it as a parameter to the new tab
-    browser.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        const currentTabUrl = tabs[0].url;
+        // Get the current tab's URL and pass it as a parameter to the new tab
+        browser.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            const currentTabUrl = tabs[0].url;
 
-        // Open a new tab with the specified URL and the current tab's URL as a parameter
-        browser.tabs.create({
-            url: `${ECO_URL}${encodeURIComponent(currentTabUrl)}`
+            // Open a new tab with the specified URL and the current tab's URL as a parameter
+            browser.tabs.create({
+                url: `${ECO_URL}${encodeURIComponent(currentTabUrl)}`
+            });
         });
     });
-});
-
+}
 
 function getImagesPathFromScore(score) {
     const DIRECTORY_PATH = "icons";
@@ -80,17 +81,17 @@ async function getCachedResult(tabId, url) {
             if (hostResults.length > 0) {
                 const {grade, score, requests} = hostResults[0];
                 console.log(`depuis l'origin c'est ${grade}`);
-                return {grade, score, requests};
+                return {grade:grade, score:score, requests:requests};
             } else {
                 throw new Error("Cannot retrieve the grade");
             }
         } else {
             console.log(grade)
-            return {grade, score, requests};
+            return {grade:grade, score:score, requests:requests};
         }
     } else {
         console.log("this is not ok man")
-        return null;
+        return {grade:null, score:null, requests:null};
     }
 }
 
@@ -108,7 +109,8 @@ async function askToComputeEvaluation(url) {
     //const ecoIndexResponse = await fetch(`https://bff.ecoindex.fr/api/tasks/${token}`);
     //const { ecoindex_result: {detail: { grade } } } = await ecoIndexResponse.json()
     //console.log(grade);
-    return grade;
+    // return grade;
+    return null;
 }
 
 /*
@@ -116,6 +118,7 @@ Each time a tab is updated, reset the page action for that tab.
 */
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
     if (tab.status == "complete" && tab.active) {
+        console.log('tab', tab.url)
         // try to get results cached in the ecoindex server
         callEcoIndex(tab.id, tab.url)
     }
