9ba06952407516d0470a0fba990011d0fe4485fd
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2023-10-13 10:19:27 +0200

    remove android / add some info

--- analysis.js
+++ analysis.js
@@ -1,114 +1,114 @@
-function updateTabUrlBar(tabId, grade, score, requests){
-  browser.pageAction.setIcon(
-    {
-      tabId,
-      path: getImagesPathFromScore(grade)
+function updateTabUrlBar(tabId, grade, score, requests) {
+    browser.pageAction.setIcon(
+        {
+            tabId,
+            path: getImagesPathFromScore(grade)
+        }
+    );
+    if (score && requests) {
+        browser.pageAction.setTitle(
+            {
+                tabId,
+                title: 'Score: ' + score + '/100, ' + requests + ' requests (source EcoIndex)'
+            }
+        );
+    } else {
+        browser.pageAction.setTitle(
+            {
+                tabId,
+                title: 'Analysis in progress...'
+            }
+        );
     }
-  );
-  if(score && requests){
-    browser.pageAction.setTitle(
-        {
-          tabId,
-          title: 'Score: ' + score + '/100, ' + requests + ' requests (source EcoIndex)'
-        }
-    );
-  }
-  else{
-    browser.pageAction.setTitle(
-        {
-          tabId,
-          title: 'Analysis in progress...'
-        }
-    );
-  }
 
-  browser.pageAction.show(tabId);
+    browser.pageAction.show(tabId);
 }
 
 browser.pageAction.onClicked.addListener((tab) => {
-  // Hide the page action icon for the current tab
-  browser.pageAction.hide(tab.id);
+    // Hide the page action icon for the current tab
+    browser.pageAction.hide(tab.id);
 
-  // Define the URL you want to open in the new tab
-  const ECO_URL = "https://bff.ecoindex.fr/redirect/?url=";
+    // Define the URL you want to open in the new tab
+    const ECO_URL = "https://bff.ecoindex.fr/redirect/?url=";
 
-  // Get the current tab's URL and pass it as a parameter to the new tab
-  browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const currentTabUrl = tabs[0].url;
+    // Get the current tab's URL and pass it as a parameter to the new tab
+    browser.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const currentTabUrl = tabs[0].url;
 
-    // Open a new tab with the specified URL and the current tab's URL as a parameter
-    browser.tabs.create({
-      url: `${ECO_URL}${encodeURIComponent(currentTabUrl)}`
+        // Open a new tab with the specified URL and the current tab's URL as a parameter
+        browser.tabs.create({
+            url: `${ECO_URL}${encodeURIComponent(currentTabUrl)}`
+        });
     });
-  });
 });
 
-function getImagesPathFromScore(score) {
-  const DIRECTORY_PATH = "icons";
-   if(score) {
-     return {
-          16: `${DIRECTORY_PATH}/16/${score}.jpg`,
-          32: `${DIRECTORY_PATH}/${score}.jpg`,
-      };
-   }
 
-  return {
-    16: `${DIRECTORY_PATH}/16/unknown.jpg`,
-    32: `${DIRECTORY_PATH}/unknown.jpg`,
-  };
+function getImagesPathFromScore(score) {
+    const DIRECTORY_PATH = "icons";
+    if (score) {
+        return {
+            16: `${DIRECTORY_PATH}/16/${score}.jpg`,
+            32: `${DIRECTORY_PATH}/${score}.jpg`,
+        };
+    }
+
+    return {
+        16: `${DIRECTORY_PATH}/16/unknown.jpg`,
+        32: `${DIRECTORY_PATH}/unknown.jpg`,
+    };
 }
 
 async function callEcoIndex(tabId, url) {
-  const { grade, score, requests } = await getCachedResult(tabId, url);
-  if(!grade) {
-    const gradeComputed = askToComputeEvaluation(tabId, url);
-    if(!gradeComputed) {
-      updateTabUrlBar(tabId, null, null, null)
+    const {grade, score, requests} = await getCachedResult(tabId, url);
+    if (!grade) {
+        const gradeComputed = askToComputeEvaluation(tabId, url);
+        if (!gradeComputed) {
+            updateTabUrlBar(tabId, null, null, null)
+        }
+        updateTabUrlBar(tabId, gradeComputed, null, null);
     }
-    updateTabUrlBar(tabId, gradeComputed, null, null);
-  }
-  updateTabUrlBar(tabId, grade, score, requests);
+    updateTabUrlBar(tabId, grade, score, requests);
 }
 
 async function getCachedResult(tabId, url) {
-  const ecoIndexResponse = await fetch(`https://bff.ecoindex.fr/api/results/?url=${url}`);
-  if(ecoIndexResponse.ok) {
-    const ecoIndexResponseObject = await ecoIndexResponse.json();
-    const { grade, score, requests } = ecoIndexResponseObject["latest-result"];
-    if(grade === "") {
-      const hostResults = ecoIndexResponseObject["host-results"];
-      if(hostResults.length > 0) {
-        const { grade, score, requests } = hostResults[0];
-        console.log(`depuis l'origin c'est ${grade}`);
-        return { grade, score, requests };
-      } else {
-        throw new Error("Cannot retrieve the grade");
-      }
+    const ecoIndexResponse = await fetch(`https://bff.ecoindex.fr/api/results/?url=${url}`);
+    if (ecoIndexResponse.ok) {
+        const ecoIndexResponseObject = await ecoIndexResponse.json();
+        const {grade, score, requests} = ecoIndexResponseObject["latest-result"];
+        if (grade === "") {
+            const hostResults = ecoIndexResponseObject["host-results"];
+            if (hostResults.length > 0) {
+                const {grade, score, requests} = hostResults[0];
+                console.log(`depuis l'origin c'est ${grade}`);
+                return {grade, score, requests};
+            } else {
+                throw new Error("Cannot retrieve the grade");
+            }
+        } else {
+            console.log(grade)
+            return {grade, score, requests};
+        }
     } else {
-      console.log(grade)
-      return { grade, score, requests };
+        console.log("this is not ok man")
+        return null;
     }
-  } else {
-    console.log("this is not ok man")
-    return null;
-  }
 }
 
 async function askToComputeEvaluation(url) {
-  // get the token
-  const tokenResponse = await fetch("https://bff.ecoindex.fr/api/tasks", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ url })
-  });
-  const token = await tokenResponse.json();
-  // try to get the task result in X seconds. If the task is not processed, then return empty grade
-  //const ecoIndexResponse = await fetch(`https://bff.ecoindex.fr/api/tasks/${token}`);
-  //const { ecoindex_result: {detail: { grade } } } = await ecoIndexResponse.json()
-  //console.log(grade);
-  return grade;
+    // get the token
+    const tokenResponse = await fetch("https://bff.ecoindex.fr/api/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({url})
+    });
+    const token = await tokenResponse.json();
+    // try to get the task result in X seconds. If the task is not processed, then return empty grade
+    //const ecoIndexResponse = await fetch(`https://bff.ecoindex.fr/api/tasks/${token}`);
+    //const { ecoindex_result: {detail: { grade } } } = await ecoIndexResponse.json()
+    //console.log(grade);
+    return grade;
 }
 
 /*
@@ -116,7 +116,7 @@ Each time a tab is updated, reset the page action for that tab.
 */
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
     if (tab.status == "complete" && tab.active) {
-      // try to get results cached in the ecoindex server
-      callEcoIndex(tab.id, tab.url)
+        // try to get results cached in the ecoindex server
+        callEcoIndex(tab.id, tab.url)
     }
 });
